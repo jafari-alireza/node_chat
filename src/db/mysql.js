9ef4991578df4da1fc65f6041a67bae7aea826f3
Alireza Jafari <jafarialireza9068@yahoo.com>
2016-08-03 13:05:14 +0430

    modified some files

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -2,18 +2,18 @@ var mysql = require("mysql");
 var cryptoJS = require("crypto-js");
 
 var cn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database : "node_chat",
+  host: "127.12.201.2",
+  user: "adminlBaueD6",
+  password: "x1-KL-Ab9X-X",
+  database : "nodejs",
   multipleStatements: true
 });
 
 var pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database : "node_chat",
+  host: "127.12.201.2",
+  user: "adminlBaueD6",
+  password: "x1-KL-Ab9X-X",
+  database : "nodejs",
   connectionLimit: 10,
   supportBigNumbers: true
 });
